2b7245961f7640154c5379b4316b03da2c671633
charming-input-2976 <mbingramm@gmail.com>
2020-11-30 22:42:16 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,9 +1,13 @@
+array = [1, "2", " ", 4, "5", " "]
+
 def count_strings(array)
   array.count do |element|
     element.String?
+  array
 end
 
 def count_empty_strings(array)
    array.count do |element|
     element.String == even?
+  array
 end
